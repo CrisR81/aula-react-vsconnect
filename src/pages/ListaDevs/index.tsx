c0968108af350cdff8ebbcafae718b4ce4dabbd1
Cristiano-Rodrigo <cristiano.rdasilva81@gmail.com>
2023-08-17 20:20:09 -0300

    atualizacao api listDEvs

--- src/pages/ListaDevs/index.tsx
+++ src/pages/ListaDevs/index.tsx
@@ -1,46 +1,55 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import CardDev from "../../components/CardDev"
 import "./style.css"
 
+import api from "../../utils/api";
+
 export default function listaDevs() {
 
-    const [devs, setDevs] = useState<any[]>([
+    const [devs, setDevs] = useState<any[]>([]);
 
-        {
-            img_perfil: "https://github.com/Thiago-Nascimento.png",
-            nome: "Thiago Nascimento",
-            email: "thiago@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/JessicaSanto.png",
-            nome: "Jessica Franzon",
-            email: "jessica@email.com",
-            skills: ["HTML", "CSS", "REACT"]
-        },
-        {
-            img_perfil: "https://github.com/odirlei-assis.png",
-            nome: "Odirlei Sabella",
-            email: "odirlei@email.com",
-            skills: ["HTML", "CSS", "ANGULAR"]
-        },
-        {
-            img_perfil: "https://github.com/alexiamelhado18.png",
-            nome: "Aléxia Vitória",
-            email: "alexia@email.com",
-            skills: ["PYTHON", "VUE", "REACT"]
-        } 
+        // {
+        //     img_perfil: "https://github.com/Thiago-Nascimento.png",
+        //     nome: "Thiago Nascimento",
+        //     email: "thiago@email.com",
+        //     skills: ["HTML", "CSS", "REACT"]
+        // },
+        // {
+        //     img_perfil: "https://github.com/JessicaSanto.png",
+        //     nome: "Jessica Franzon",
+        //     email: "jessica@email.com",
+        //     skills: ["HTML", "CSS", "REACT"]
+        // },
+        // {
+        //     img_perfil: "https://github.com/odirlei-assis.png",
+        //     nome: "Odirlei Sabella",
+        //     email: "odirlei@email.com",
+        //     skills: ["HTML", "CSS", "ANGULAR"]
+        // },
+        // {
+        //     img_perfil: "https://github.com/alexiamelhado18.png",
+        //     nome: "Aléxia Vitória",
+        //     email: "alexia@email.com",
+        //     skills: ["PYTHON", "VUE", "REACT"]
+        // } 
   
-    ]);
+    //]);
 
     const [skillDigitada, setSkillDigitada] = useState<string>("");
 
     const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs);
 
+
+    useEffect( () => {
+document.title = "VS Connect - Lista de Devs"
+
+    listarDesenvolvedores()
+    }, [] )
+
     function buscarPorSkill(event: any){
         event.preventDefault(); 
 
-        const devsFiltrados = devs.filter((dev: any) => dev.skills.includes(skillDigitada.toLocaleUpperCase) )
+        const devsFiltrados = devs.filter((dev: any) => dev.skills.includes(skillDigitada.toLocaleUpperCase()) )
 
         if(devsFiltrados.length === 0){
             alert("Nenhum desenvolvedor com essa Skill foi encontrado")
@@ -56,6 +65,14 @@ export default function listaDevs() {
         setSkillDigitada(event.target.value)
     }
 
+    function listarDesenvolvedores() {
+
+        api.get("users").then((response: any) => {
+            console.log(response.data)
+            setDevs(response.data)
+        } )
+
+    }
 
     return (
 <main id="lista-devs">
@@ -74,8 +91,8 @@ export default function listaDevs() {
                     </form>
                     <div className="wrapper_lista">
                         <ul>
-                            {listaDevsFiltrados.map((dev: any, index: number) => {
-                                return <li>
+                            {devs.map((dev: any, index: number) => {
+                                return <li key={index}>
                                     <CardDev 
                                      foto={dev.img_perfil}
                                      nome={dev.nome}
